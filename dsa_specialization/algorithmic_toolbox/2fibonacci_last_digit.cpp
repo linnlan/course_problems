551fd40a9e656e1da5bb4ab642f744367a381673
Subinoy Karmakar <subinoy2001@gmail.com>
2020-08-17 21:29:39 +0530

    Create 2fibonacci_last_digit.cpp

---
+++ dsa_specialization/algorithmic_toolbox/2fibonacci_last_digit.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int get_fibonacci_last_digit_naive(int n) {
+    if (n <= 1)
+        return n;
+
+    int previous = 0;
+    int current  = 1;
+
+    for (int i = 0; i < n - 1; ++i) {
+        int tmp_previous = previous;
+        previous = current;
+        current = tmp_previous + current;
+    }
+
+    return current % 10;
+}
+int get_fib_last_digit(int n)
+{
+    int f[n+2];
+    f[0]=0;
+    f[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        f[i] = (f[i-1]+f[i-2])%10;
+    }
+    return f[n];
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    int c = get_fib_last_digit(n);
+    std::cout << c << '\n';
+    }
